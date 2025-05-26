c5f9f917af2ffdd5afdce4241b64ec3db1d62c9b
Carlo Dulutan <202310776@fit.edu.ph>
2025-05-26 06:54:50 -0700

    feat(router.php) added router.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'carlo-dulutan/ad-task-1',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e73ca522bc4dbc75cbd8c844bddc2cbff14bf88e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'carlo-dulutan/ad-task-1' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e73ca522bc4dbc75cbd8c844bddc2cbff14bf88e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
